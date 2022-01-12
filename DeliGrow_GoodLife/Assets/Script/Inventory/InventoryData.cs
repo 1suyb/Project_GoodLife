2f8f9a0e5d2b36356eefc3a1179785eee92e5118
1suyb <suyb1234@gmail.com>
2022-01-13 00:21:38 +0900

    2021 1 13

---
+++ DeliGrow_GoodLife/Assets/Script/Inventory/InventoryData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InventoryData : MonoBehaviour
+{
+    public int datatest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static void test(){
+        Debug.Log("hi");
+    }
+        
+}
---
+++ DeliGrow_GoodLife/Assets/Script/Inventory/InventoryUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+        // Start is called before the first frame update
+    void Start()
+    {
+         InventoryData.test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
