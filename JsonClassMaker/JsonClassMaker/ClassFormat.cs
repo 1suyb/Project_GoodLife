c8ca3ee5c921747af47c6f99dbdb592f9586d359
SuyeonBak <89519957+1suyb@users.noreply.github.com>
2022-03-03 01:34:07 +0900

    2022.3.3
    DataClass 생성 자동화 진행 중.
    진행내역
    1. jsondata 역직렬화
    2. 자동화 string format 생성
    
    해야할 내역
    1. json 데이터에서 데이터 스키마 파싱 혹은 xml데이터 생성
    2. 데이터 스키마 이용해서 format조립 프로그램 작성
    3. enum 데이터 포맷 작성
    4. enum 데이터 조립
    5. 테스트

---
+++ JsonClassMaker/JsonClassMaker/ClassFormat.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonClassMaker
+{
+    class StructFormat
+    {
+        // {0} : 데이터 타입 이름 / 번호 목록
+        // {1} : JsonUtil 목록
+        // {2} : class 목록
+        public static string fileFormat =
+@"
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DataType
+{{
+    {0}
+}}
+
+public static class JsonUtil
+{{
+    {1}
+}}
+
+public class Table
+{{
+    public int ID;
+}}
+{2}
+
+";
+        // {0} : type 이름
+        // {1} : type 번호
+        public static string enumFormat =
+@"
+{0} = {1};
+";
+        #region JsonUtil
+        // {0} : case 문
+        public static string jsonUtilConverTypeFormat =
+@"
+    private static string ConvertType(DataType datatype)
+    {{
+        string filename;
+        switch(datatype){
+         {0}
+        default :
+            filename = "";
+            break;
+        }
+        return filename;
+    }}
+;";
+        // {0} : type 이름 (대문자)
+        // {1} : type에 따른 파일이름 (DataTable 이름)
+        public static string jsonUtilCaseFormat =
+@"
+    case {0} : 
+        filename = {1};
+        break;
+";
+        // {0} : DataTable 이름
+        public static string jsonUtilDeserializeFormat =
+@"
+    public static List<{0}> {0}Deserialize(DataType datatype)
+    {{
+        String{0}[] t = JsonHelper.Deserialize<String{0}>(ConvertType(datatype));
+        List<{0}> l = String{0}.Convert(t);
+        return l;
+    }}
+";
+        #endregion
+
+        #region Class
+        // {0} : DataTable이름
+        // {1} : 맴버 변수
+        // {2} : 매개 변수
+        // {3} : 변수 초기화 부분
+        // {4} : 문자열 클래스 맴버 변수
+        // {5} : 생성자 초기화 매개변수
+        public static string classFormat =
+@"
+public class {0} : Table
+{{
+    {1}
+    public {0}({2})
+    {{
+        {3}
+    }}
+}}
+
+public class String{0}
+{{
+    {4}
+
+    public static List<{0}> Convert(String{0}[] table)
+    {{
+        List<{0}> t = new List<{0}>();
+        foreach (Stirng{0} item in table)
+        {{
+            t.Add(new {0}({5});
+        }}
+        return t;
+    }}
+}}
+";
+        // {0} : 변수
+        public static string fullvalueFormat = 
+@"
+public {0};
+";
+        // {0} : 변수 형식
+        // {1} : 변수 이름
+        public static string valueFormat = 
+@"
+{0} {1}
+";
+        #endregion
+    }
+}
---
+++ JsonClassMaker/JsonClassMaker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace JsonClassMaker
+{
+    class hi { }
+    class Program
+    {
+        static string genClass;
+        static string dataTypeEnums;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        
+    }
+}
