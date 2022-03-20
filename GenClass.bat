1b9127b86e1e398b2b0b74f4f90fb55c346ca1d4
SuyeonBak <89519957+1suyb@users.noreply.github.com>
2022-03-20 21:58:51 +0900

    2022.3.20
    파일 위치 이동

--- GenClass.bat
+++ GenClass.bat
@@ -1,3 +1,3 @@
-START JsonClassMaker/JsonClassMaker/bin/Debug/netcoreapp3.1/JsonClassMaker.exe DataSchema.xml
+START /Utility/JsonClassMaker/JsonClassMaker/bin/Debug/netcoreapp3.1/JsonClassMaker.exe DataSchema.xml
 XCOPY /Y GenClass.cs "./DeliGrow_GoodLife/Assets/Script"
 XCOPY /Y GenUtil.cs "./DeliGrow_GoodLife/Assets/Script"
