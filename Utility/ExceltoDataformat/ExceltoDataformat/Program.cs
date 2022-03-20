74eb1f4aa9ee6f39efd8236b37655b8cef775cd0
SuyeonBak <89519957+1suyb@users.noreply.github.com>
2022-03-20 23:24:58 +0900

    2022.3.20
    엑셀파일 읽기

--- Utility/ExceltoDataformat/ExceltoDataformat/Program.cs
+++ Utility/ExceltoDataformat/ExceltoDataformat/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
 
-namespace ExceltoDataformat
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExceltoJson
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string filePath = "";
+            string readfilename = System.IO.Directory.GetCurrentDirectory() + "\\table.xlsx";
+            string writeJsonFileName = "table.json";
+            string writeXmlFileName = "table.xml";
+
+            if (args.Length > 1)
+            {
+                filePath = args[0];
+                readfilename = args[1];
+            }
+
+            Excel.Application excel = new Excel.Application();
+            Workbook workbook = excel.Workbooks.Open(readfilename);
+            Worksheet worksheet = null;
+            Console.WriteLine(workbook.Name);
+            for (int i = 1; i < workbook.Worksheets.Count+1; i++)
+            {
+                worksheet = workbook.Worksheets.Item[i];
+                Console.WriteLine(worksheet.Name);
+                if (worksheet.Name.Contains("Table"))
+                {
+                    WriteJsonFile(worksheet);
+                }
+            }
+
+            excel.Quit();
         }
+
+        public static void WriteJsonFile(Worksheet worksheet)
+        {
+            List<string> column = new List<string>();
+            Excel.Range range = worksheet.UsedRange;
+            for(int i = 1; i < range.Columns.Count+1; i++)
+            {
+                column.Add((range.Cells[1, i] as Excel.Range).Value2.ToString());
+                Console.WriteLine(column[i-1]);
+            }
+        }
+
+        public void WriteXmlFile()
+        {
+
+        }
+        
+
     }
 }
