905db721e141cd9605ee0bb5b6e75a69513026c7
SuyeonBak <89519957+1suyb@users.noreply.github.com>
2022-03-27 23:03:03 +0900

    90퍼완성
    적당히 데이터 뽑힘

--- Utility/ExceltoDataformat/ExceltoDataformat/Program.cs
+++ Utility/ExceltoDataformat/ExceltoDataformat/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Microsoft.Office.Interop.Excel;
 
 using Excel = Microsoft.Office.Interop.Excel;
@@ -12,15 +13,13 @@ namespace ExceltoJson
         public static string XmlText;
         static void Main(string[] args)
         {
-            string filePath = "";
+            string readfilePath = "";
             string readfilename = System.IO.Directory.GetCurrentDirectory() + "\\table.xlsx";
             string outfilePath = "";
-            string writeJsonFileName = "table.json";
-            string writeXmlFileName = "table.xml";
 
             if (args.Length > 2)
             {
-                filePath = args[0];
+                readfilePath = args[0];
                 readfilename = args[1];
                 outfilePath = args[2];
             }
@@ -32,62 +31,113 @@ namespace ExceltoJson
             for (int i = 1; i < workbook.Worksheets.Count+1; i++)
             {
                 worksheet = workbook.Worksheets.Item[i];
-                Console.WriteLine(worksheet.Name);
-
-                if (worksheet.Name.Contains("Table"))
+                string worksheetname = worksheet.Name;
+                Console.WriteLine("===========================================================");
+                Console.WriteLine(worksheetname);
+                if (worksheetname.Contains("Table"))
                 {
+                    
                     string column;
                     string type;
+                    string data;
                     List<string> columns = new List<string>();
-                    List<string> data = new List<string>();
+                    List<string> datas = new List<string>();
                     Excel.Range range = worksheet.UsedRange;
                     for (int j = 1; j < range.Columns.Count + 1; j++)
                     {
-                        column = (range.Cells[1, i] as Excel.Range).Value2.ToString();
-                        type = (range.Cells[2, i] as Excel.Range).Value2.ToString();
+                        // 칼럼 및 데이터 형식 가져오기
+                    }
+                    for (int j = 3; j < range.Rows.Count + 1; j++)
+                    {
+                        // 데이터 한줄
+                        for (int k = 1; k < range.Columns.Count + 1; k++)
+                        {
+                            // 데이터 하나
+                            if ((range.Cells[j, k] as Excel.Range).Value2 != null)
+                            {
+                                
+                            }
+                           
+                        }
+                    }
+                    //데이터 완성
+
+
+
+
+
+
+
+                    for (int j = 1; j < range.Columns.Count + 1; j++)
+                    {
+                        column = (range.Cells[1, j] as Excel.Range).Value2.ToString();
+                        type = (range.Cells[2, j] as Excel.Range).Value2.ToString();
                         XmlText += string.Format(StringFormat.XmlDataFormat, type, column);
                         columns.Add(column);
+                        //Console.WriteLine(column);
                     }
-                    for(int j = 1;j<range.Rows.Count + 1; j++)
+                    string jsondata = "";
+                    for(int j = 3;j < range.Rows.Count + 1; j++)
                     {
-                        for(int k = 1; k<range.Columns.Count +1; k++)
+                        Console.WriteLine("로우갯수");
+                        Console.WriteLine(range.Rows.Count);
+                        Console.WriteLine("===========================");
+                        for(int k = 1; k < range.Columns.Count +1; k++)
                         {
-                            data.Add(range.Cells[j, k]);
+                            if ((range.Cells[j, k] as Excel.Range).Value2 != null)
+                            {
+                                if (k != 1)
+                                {
+                                    jsondata += ",\n";
+                                }
+                                data = (range.Cells[j, k] as Excel.Range).Value2.ToString();
+                                Console.Write(data);
+                                jsondata += string.Format(StringFormat.JsonDataFormat, columns[k-1], data);
+                            }
+                            Console.Write("\n");
                         }
-                        WriteJsonText(columns, data);
+                        JsonText += string.Format(StringFormat.JsonFormat, jsondata);
+                        jsondata = "";
+                        datas.Clear(); ;
                     }
-                    WriteJsonFile();
-                    WriteXmlFile();
-                    JsonText = "";
-                    XmlText = "";
+                    WriteJsonFile(outfilePath,worksheetname);
+                    WriteXmlFile(outfilePath,worksheetname);
+                    
+                    
                 }
             }
 
             excel.Quit();
+            //Marshal.ReleaseComObject();
         }
 
-        public static void WriteJsonText(List<string> columns,List<string> data)
+        public static void WriteJsonText(List<string> columns,List<string> datas)
         {
             string jsondata = "";
-            for(int i = 0; i < data.Count; i++)
+            for(int i = 0; i < datas.Count; i++)
             {
-                jsondata += string.Format(StringFormat.JsonDataFormat, columns[i], data[i]) + "\n";
+                jsondata += string.Format(StringFormat.JsonDataFormat, columns[i], datas[i]) + "\n";
             }
             JsonText += string.Format(StringFormat.JsonFormat, jsondata) + "\n";
 
         }
 
-        public static void WriteXmlText(string column, string type)
+        public static void WriteXmlText(string table, string data)
         {
             
         }
-        public static void WriteJsonFile()
+        public static void WriteJsonFile(string outfilepath,string table)
         {
+           
+            System.IO.File.WriteAllText(outfilepath + table + ".json",JsonText);
 
+            JsonText = "";
         }
-        public static void WriteXmlFile()
+        public static void WriteXmlFile(string outfilePath,string table)
         {
-
+            XmlText = string.Format(StringFormat.XmlFormat, table, XmlText);
+            System.IO.File.WriteAllText(outfilePath + table + ".xml", XmlText);
+            XmlText = "";
         }        
 
     }
--- Utility/ExceltoDataformat/ExceltoDataformat/StringFormat.cs
+++ Utility/ExceltoDataformat/ExceltoDataformat/StringFormat.cs
@@ -10,29 +10,35 @@ namespace ExceltoJson
         public static string JsonFormat =
 @"
 [
+{0}
+]
+";
+        // {0} : 데이터들
+        public static string JsonDatasFormat = 
+@"
 {{
 {0}
 }}
-]
 ";
         // {0} : 칼럼네임
         // {1} : 값
         public static string JsonDataFormat =
-@"""{0}"" : ""{1}""";
+@"  ""{0}"" : ""{1}""";
 
         // {0} : table name
         // {1} : Datas
         public static string XmlFormat =
 @"<?xml version=""1.0"" encoding=""utf-8"" ?>
 <DataSchema>
-    <table name = {0}>
-        {1}
+    <table name = ""{0}"">
+{1}
     </table>
 </DataSchema>
 ";
         // {0} : 데이터 형식
         // {1} : 칼럼네임
         public static string XmlDataFormat =
-@"{0} name = ""{1}""";
+@"      <{0} name = ""{1}""/>
+";
     }
 }
